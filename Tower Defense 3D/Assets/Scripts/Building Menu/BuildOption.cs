921dc8ad2b8fb290421ed4b2657315c65c2d60c5
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2020-10-04 13:27:44 +0200

    added data classes for build option and build category

--- Tower Defense 3D/Assets/Scripts/Building Menu/BuildOption.cs
+++ Tower Defense 3D/Assets/Scripts/Building Menu/BuildOption.cs
@@ -5,8 +5,19 @@ public class BuildOption : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] GameObject buildPrefab;
 
+    private GameObject _buildPrefab;
+
+    public void Initialize(BuildOptionData buildOptionData)
+    {
+        GetComponent<SpriteRenderer>().sprite = buildOptionData.BuildIcon;
+        _buildPrefab = buildOptionData.BuildPrefab;
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
+        if (buildPrefab == null)
+            return;
+
         EventManager.ExecuteEvent<IBuildOptionClicked>((x, y) => x.OnBuildOptionClicked(buildPrefab));
     }
 }
---
+++ Tower Defense 3D/Assets/Scripts/Building Menu/BuildOptionData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Build Option Data", menuName = "Data/Build Option Data")]
+public class BuildOptionData : ScriptableObject
+{
+    [SerializeField] GameObject buildPrefab;
+    [SerializeField] Sprite buildIcon;
+
+    public GameObject BuildPrefab => buildPrefab;
+    public Sprite BuildIcon => buildIcon;
+}
