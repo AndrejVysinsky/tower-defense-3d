1acb81e8d9f6a0e809d21a4dc7d810f2886afee5
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2020-10-15 15:35:04 +0200

    added start and end portal

---
+++ Tower Defense 3D/Assets/GameController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] MapSaveManager saveManager;
+
+    private void Awake()
+    {
+        saveManager.LoadMapData();
+    }
+}
