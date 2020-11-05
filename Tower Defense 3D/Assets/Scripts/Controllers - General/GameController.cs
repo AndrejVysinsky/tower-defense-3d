36f2b9acaf94f87d71e36ac6614c89e4fa7c7699
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2020-11-05 13:27:35 +0100

    added game info (currency, wave, lives) + ui display

---
+++ Tower Defense 3D/Assets/Scripts/Controllers - General/GameController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance { get; private set; }
+
+    [SerializeField] GameInfoDisplay gameInfoDisplay;
+    //[SerializeField] GameOver gameOver;
+    [SerializeField] int startingCurrency;
+    [SerializeField] int startingLives;
+
+    public int Currency { get; private set; }
+    public int Lives { get; private set; }
+
+    private int _wave;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        Currency = startingCurrency;
+        Lives = startingLives;
+
+        gameInfoDisplay.UpdateCurrencyText(startingCurrency);
+        gameInfoDisplay.UpdateWaveText(_wave);
+        gameInfoDisplay.UpdateLivesText(startingLives);
+    }
+
+    public void ModifyCurrencyBy(int value)
+    {
+        UpdateCurrency(value);
+    }
+
+    public void ModifyCurrencyBy(int value, Vector3 position)
+    {
+        gameInfoDisplay.DisplayCurrencyChangeText(value, position);
+        UpdateCurrency(value);
+    }
+
+    private void UpdateCurrency(int value)
+    {
+        Currency += value;
+        gameInfoDisplay.UpdateCurrencyText(Currency);
+
+        //EventManager.Instance.ExecuteEvent<ICurrencyChanged>((x, y) => x.OnCurrencyChanged(Currency));
+    }
+
+    public void ModifyLivesBy(int value)
+    {
+        UpdateLives(value);
+    }
+
+    public void ModifyLivesBy(int value, Vector3 position)
+    {
+        gameInfoDisplay.DisplayLivesChangeText(value, position);
+        UpdateLives(value);
+    }
+
+    private void UpdateLives(int value)
+    {
+        Lives += value;
+        gameInfoDisplay.UpdateLivesText(Lives);
+
+        //if (Lives <= 0)
+        //{
+        //    if (gameOver.gameObject.activeSelf)
+        //        return;
+
+        //    Time.timeScale = 0f;
+
+        //    gameOver.gameObject.SetActive(true);
+
+        //    gameOver.InitializeScore(_wave, 100, false);
+        //    // gameOver.InitializeScore(250, 3000, true);
+        //}
+    }
+
+    public void WaveSpawned(int waveNumber)
+    {
+        _wave = waveNumber;
+
+        gameInfoDisplay.UpdateWaveText(waveNumber);
+    }
+}
---
+++ Tower Defense 3D/Assets/Scripts/Controllers - General/GameInfoDisplay.cs
@@ -0,0 +1,53 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GameInfoDisplay : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI currencyText;
+    [SerializeField] TextMeshProUGUI waveText;
+    [SerializeField] TextMeshProUGUI livesText;
+
+    public void UpdateCurrencyText(int total)
+    {
+        currencyText.text = GetParsedCurrency(total);
+    }
+
+    private string GetParsedCurrency(float value)
+    {
+        char thousandsChar = default;
+
+        if (value / 1000000 >= 1)
+        {
+            value /= 1000000;
+            thousandsChar = 'm';
+        }
+        else if (value / 1000 >= 1)
+        {
+            value /= 1000;
+            thousandsChar = 'k';
+        }
+
+        return value.ToString("0.##", System.Globalization.CultureInfo.InvariantCulture) + thousandsChar;
+    }
+
+    public void DisplayCurrencyChangeText(int change, Vector3 position)
+    {
+        //TODO: spawn text at position
+    }
+
+    public void UpdateLivesText(int total)
+    {
+        //TODO: spawn text at position
+        livesText.text = total.ToString();
+    }
+
+    public void DisplayLivesChangeText(int change, Vector3 position)
+    {
+        //TODO: spawn text at position
+    }
+
+    public void UpdateWaveText(int waveNumber)
+    {
+        waveText.text = waveNumber.ToString();
+    }
+}
