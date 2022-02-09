0a96cad55d05b654864b08d6f961de10a78b5fb8
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2022-02-09 18:25:16 +0100

    added copy lobby id button

---
+++ Tower Defense 3D/Assets/CopyButton.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class CopyButton : MonoBehaviour
+{
+    [SerializeField] TMP_InputField input;
+    
+    public void CopyToClipboard()
+    {
+        GUIUtility.systemCopyBuffer = input.text;
+    }
+}
